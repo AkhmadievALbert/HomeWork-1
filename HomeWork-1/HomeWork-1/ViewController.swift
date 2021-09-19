fb2965618b9519521f9a3383a322fc812ecea382
AkhmadievALbert <a.akhmadiev@tinkoff.ru>
2021-09-19 20:32:35 +0300

    [init] Swift-project

---
+++ HomeWork-1/HomeWork-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork-1
+//
+//  Created by a.akhmadiev on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
